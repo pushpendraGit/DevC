ebfded591e0bb8d4ca5115f27b16f2fb16a508a1
PushpendraMentora <pushpendra@mentora.institute>
2021-03-07 00:06:45 +0530

    ejs is set as view engine

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.use('views', './views');
+
 app.listen(port, function (err) {
     if (err) {
         console.log('Hi DevC there is an error while creating the express server');
